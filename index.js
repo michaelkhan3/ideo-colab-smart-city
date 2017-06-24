3a90a89df84885dde3cb6bd1c9ab4bef5cf9f10c
Matt Hunt <mstephenhunt@gmail.com>
2017-06-24 08:16:39 -0400

    using express path to open index.html

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
