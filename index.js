f1759614e45805375275ccd69708891ea3e45435
Matt Hunt <mstephenhunt@gmail.com>
2017-06-24 09:57:08 -0400

    using socket.io sample implementation

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require('express')
-const app = require('express')()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const path = require('path')
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
+// Serve the CSS as a static asset
 app.use(express.static(__dirname + '/public'))
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html')
 })
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
+io.on('connection', function(socket){
+  console.log('a user connected')
+})
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
-
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
+http.listen(3000, function (){
+  console.log('listening on *:3000')
+})
