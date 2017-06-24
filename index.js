76bde619e3faf54c7c8eb54c236a498a4fd8b3f9
Matt Hunt <mstephenhunt@gmail.com>
2017-06-24 09:51:38 -0400

    requiring socketio in index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const app = express()
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 const path = require('path')
 
 app.use(express.static(__dirname + '/public'))
@@ -12,3 +14,15 @@ app.get('/',function(req,res){
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
